72717430ee24db1a5ba393d741bb8f1cf3a2bc6a
Luke O'Sullivan <luke@Lukes-MacBook.local>
2019-01-31 13:23:07 -0500

    update

--- server.js
+++ server.js
@@ -12,3 +12,37 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Get all tables that aren't waiting
+app.get("/api/views", function (req, res) {
+  connection.query("SELECT * FROM tables WHERE isWaiting = FALSE", function (err, dbTables) {
+    res.json(dbTables);
+  });
+});
+
+// Save a new table
+// Set isWaiting to true if there are already 5 or more "seated" tables
+app.post("/api/views", function(req, res) {
+  connection.query("SELECT COUNT (IF(isWaiting = FALSE, 1, NULL)) FROM tables", function (err, dbSeated) {
+    if (err) throw err;
+
+    if (dbSeated[0].count > 4) {
+      req.body.isWaiting = true;
+   }
+   
+   connection.query("INSERT INTO tables SET ?", req.body, function (err, result) {
+    if (err) throw err;
+
+    connection.query("SELECT * FROM tables WHERE id = ?", [result.insertId], function (err, dbTables) {
+      res.json(dbTables[0]);
+    });
+   });
+
+  });
+});
+
+// Get all tables where isWaiting is true (waiting list)
+app.get("api/waitlist", function(req, res) {
+  connection.query("SELECT * FROM tables WHERE isWaiting = TRUE", function(err, dbTables) {
+    res.json(dbTables);
+    
+  });
+});
